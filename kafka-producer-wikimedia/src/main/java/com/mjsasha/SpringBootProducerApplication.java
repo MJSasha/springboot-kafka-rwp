5d76be0682c8b5a53c59067d1a5b899778f5d615
MJSasha <sasha.ushankov@gmail.com>
2023-02-24 19:41:54 +0300

    Remove unused code

--- kafka-producer-wikimedia/src/main/java/com/mjsasha/SpringBootProducerApplication.java
+++ kafka-producer-wikimedia/src/main/java/com/mjsasha/SpringBootProducerApplication.java
@@ -1,8 +1,6 @@
 package com.mjsasha;
 
-import com.mjsasha.handler.WikimediaChangesHandler;
 import com.mjsasha.kafka.WikimediaKafkaProducer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
